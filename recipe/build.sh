ce8e9a4a66aa166494e78c6fa3b98d87bbaf84ec
Michael Sarahan <msarahan@gmail.com>
2017-08-28 11:52:47 -0500

    graft mkl onto conda-forge recipe.  Use cb3.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+cp $RECIPE_DIR/test_fft.py numpy/fft/tests
+
+if [ "$blas_impl" = "mkl" ]; then
+
+cat > site.cfg <<EOF
+[mkl]
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+mkl_libs = mkl_rt
+lapack_libs =
+
+EOF
+
+printf "\n\n__mkl_version__ = \"$mkl\"\n" >> numpy/__init__.py
+
+else # Non-MKL
+
 cat > site.cfg <<EOF
 [DEFAULT]
 library_dirs = $PREFIX/lib
@@ -16,6 +33,11 @@ include_dirs = $PREFIX/include
 
 EOF
 
+<<<<<<< HEAD
+=======
+fi
+
+>>>>>>> graft mkl onto conda-forge recipe.  Use cb3.
 $PYTHON setup.py config
 $PYTHON setup.py build
 $PYTHON setup.py install
