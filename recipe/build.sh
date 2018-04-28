0b5bb9e9c6cd0dcba590191e19933381871f12ac
Ray Donnelly <mingw.android@gmail.com>
2018-04-28 15:35:25 -0700

    Ensure Cython recreates mtrand.c (by deleting it)

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,9 @@ if [ "$blas_impl" = "mkl" ]; then
     printf "\n\n__mkl_version__ = \"$mkl\"\n" >> numpy/__init__.py
 fi
 
+# https://github.com/numpy/numpy/issues/9391
+find . -name mtrand.c -exec rm {} \;
+
 # site.cfg comes from blas devel package (e.g. mkl-devel)
 cp $PREFIX/site.cfg site.cfg
 
