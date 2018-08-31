f3ebe21206a7f1149ff33711f2f52a94c910de70
Ray Donnelly <mingw.android@gmail.com>
2018-08-30 21:40:50 -0400

    Re-cythonize

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 cp $RECIPE_DIR/test_fft.py numpy/fft/tests
 
 if [ "$blas_impl" = "mkl" ]; then
@@ -12,11 +14,20 @@ fi
 # site.cfg comes from blas devel package (e.g. mkl-devel)
 cp $PREFIX/site.cfg site.cfg
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
-    export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup"
+# site.cfg should not be defined here.  It is provided by blas devel packages (either mkl-devel or openblas-devel)
+# Urgh ..
+export CFLAGS="${CFLAGS} -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-missing-field-initializers"
+if [[ ${target_platform} == osx-64 ]]; then
+    export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
+else
+    export LDFLAGS="$LDFLAGS -shared"
+    export FFLAGS="$FFLAGS -Wl,-shared"
 fi
 
+if [[ ${DEBUG_PY} == yes ]]; then
+  DBG="--debug"
+fi
 
-$PYTHON setup.py config
-$PYTHON setup.py build # -j ${CPU_COUNT}
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+${PYTHON} setup.py config
+${PYTHON} setup.py build ${DBG}
+${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
