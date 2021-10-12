58da3c16f498f269b0f46eaab0953bcc8a6a618a
Paul Yim <paul.j.yim@gmail.com>
2021-10-12 17:28:40 +0000

    save

--- recipe/install_base.sh
+++ recipe/install_base.sh
@@ -2,10 +2,17 @@
 
 set -e
 
-# site.cfg is provided by blas devel packages (either mkl-devel or openblas-devel)
-case $( uname -m ) in
-aarch64) cp $PREFIX/aarch_site.cfg site.cfg;;
-*)       cp $PREFIX/site.cfg site.cfg;;
-esac
+# For mkl and openblas BLAS variants, the site.cfg file is provided
+# by the devel packages (either mkl-devel or openblas-devel). See:
+#   intel_repack-feedstock/recipe/install-devel.sh
+#   openblas-feedstock/recipe/build.sh
+
+# For armpl BLAS variant, copy over the site.cfg file from recipe.
+#if [[ `uname -m` == aarch64 ]]; then
+#  cp "${RECIPE_DIR}"/aarch64_armpl_site.cfg "${PREFIX}"/site.cfg;
+#fi
+
+#echo "CONFIG!"
+#${PYTHON} setup.py config
 
 ${PYTHON} -m pip install --no-deps --ignore-installed -v .
