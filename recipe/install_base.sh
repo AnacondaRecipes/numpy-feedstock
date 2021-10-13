ee4ebbc0760ee11ec6abf9428f3aa09abbef1c2c
Paul Yim <paul.j.yim@gmail.com>
2021-10-13 22:07:27 +0000

    Remove condition for aarch64; the site.cfg should come from the BLAS package (for all platforms)

--- recipe/install_base.sh
+++ recipe/install_base.sh
@@ -2,17 +2,11 @@
 
 set -e
 
-# For mkl and openblas BLAS variants, the site.cfg file is provided
-# by the devel packages (either mkl-devel or openblas-devel). See:
+# To build the different BLAS variants, the site.cfg file is provided
+# by the BLAS packages (mkl-devel, openblas-devel, or armpl). See:
 #   intel_repack-feedstock/recipe/install-devel.sh
 #   openblas-feedstock/recipe/build.sh
-
-# For armpl BLAS variant, copy over the site.cfg file from recipe.
-#if [[ `uname -m` == aarch64 ]]; then
-#  cp "${RECIPE_DIR}"/aarch64_armpl_site.cfg "${PREFIX}"/site.cfg;
-#fi
-
-#echo "CONFIG!"
-#${PYTHON} setup.py config
+#   armpl-feedstock/recipe/repack_armpl.sh
+cp $PREFIX/site.cfg site.cfg
 
 ${PYTHON} -m pip install --no-deps --ignore-installed -v .
