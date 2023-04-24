f1816fee938c9a678cae796f4bbd0cdb115e98b8
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2023-04-24 13:10:25 +0200

    use c11 for windows

--- recipe/install_base.bat
+++ recipe/install_base.bat
@@ -2,7 +2,7 @@
 
 COPY %PREFIX%\site.cfg site.cfg
 
-%PYTHON% -m pip install --no-deps --ignore-installed -v .
+%PYTHON% -m pip install --no-deps --ignore-installed  -v .
 if errorlevel 1 exit 1
 
 XCOPY %RECIPE_DIR%\f2py.bat %SCRIPTS% /s /e
