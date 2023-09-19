383ed7d02949703aa3d0cd5823ae2dd26ae9e8a8
Charles Bousseau <cbousseau@anaconda.com>
2023-09-19 16:55:16 -0400

    add back disabling of cpu optimmizations for ppc

--- recipe/install_base.sh
+++ recipe/install_base.sh
@@ -4,6 +4,17 @@ set -ex
 
 cd ${SRC_DIR}
 
+UNAME_M=$(uname -m)
+case "$UNAME_M" in
+    ppc64*)
+        # Optimizations trigger compiler bug.
+        EXTRA_OPTS="-Dcpu-dispatch=min"
+        ;;
+    *)
+        EXTRA_OPTS=""
+        ;;
+esac
+
 if [[ ${blas_impl} == openblas ]]; then
     BLAS=openblas
 else
@@ -16,5 +27,6 @@ $PYTHON -m build -w -n -x \
     -Cbuilddir=builddir \
     -Csetup-args=-Dblas=${BLAS} \
     -Csetup-args=-Dlapack=${BLAS} \
+    $EXTRA_OPTS \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
 $PYTHON -m pip install dist/numpy*.whl
